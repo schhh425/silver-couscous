6d1ff90d636992c451474e285a1dcb30e6742880
zhengwu.li <641876684@qq.com>
2019-11-04 20:15:58 +0800

    readMe

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-苏苏苏苏
+## Prepare:
+
+develop Environment:
+    jdk1.8
+    maven 
+    
+
+## TASK 
+
+The sample uses Maven. It can be built and run from the command line:
+
+----
+$ mvn spring-boot:run
+----
+
+Web Services
+    http://localhost:8081/sch_service/Hello?WSDL will now display the generated WSDL.
+
+
+
+RestFul server
+    http://localhost:8081/hello/{name}    replace you name
+you will client me e.g  http://localhost:8081/hello/suchunhao
+response is this:
+    GET  i will say "you get me" 
+    PUT  i will say "you put me" 
+    DELETE   i will say "goodbye " 
+    POST   i will say "you post me " 
+    
--- src/main/java/com/example/demo/myserver/controller/MyRestServerController.java
+++ src/main/java/com/example/demo/myserver/controller/MyRestServerController.java
@@ -1,14 +1,32 @@
 package com.example.demo.myserver.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestServerController {
 
 	@GetMapping("/hello/{name}")
-	public String hellWord(@PathVariable("name") String name) {
-		return "hello" + name;
+	public String hellWordGet(@PathVariable("name") String name) {
+		return "hello:" + name + " you get me";
+	}
+
+	@PostMapping("/hello/{name}")
+	public String hellWordPost(@PathVariable("name") String name) {
+		return "hello:" + name + " you post me,i will remember you ";
+	}
+
+	@DeleteMapping("/hello/{name}")
+	public String hellWordDelete(@PathVariable("name") String name) {
+		return "goodbye:" + name;
+	}
+
+	@PutMapping("/hello/{name}")
+	public String hellWordPut(@PathVariable("name") String name) {
+		return "hello:" + name + " you put me";
 	}
 }
